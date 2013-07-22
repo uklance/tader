c604890c5aee54d38ef7515ad4396e4e859f5881
Lance <uklance@gmail.com>
2013-07-22 11:19:14 +0100

    Update Grater.java

--- grater-core/src/main/java/org/grater/Grater.java
+++ grater-core/src/main/java/org/grater/Grater.java
@@ -4,18 +4,16 @@ import java.util.Map;
 
 public interface Grater {
 	TableRow insert(TableRow row);
-    TableRow insert(String table, Map<String, Object> fields);
-    TableRow insert(String table, Object... columnsAndValues);
-	public abstract TableRow select(String name, Map<String, Object> values);
+	TableRow insert(String table, Map<String, Object> fields);
+	TableRow insert(String table, Object... columnsAndValues);
+	TableRow select(String name, Map<String, Object> values);
     
-    /*
-    <T extends Entity> insert(Class<T> type, Map fields);
-    <T extends Entity> T insert(T fields);
-
-    boolean delete(String table, Object pk);
-    boolean delete(Entity entity);
-
-    int deleteAll(String table, Map fields);
-    int deleteAll(Entity fields);
-    */
+	/*
+	<T extends Entity> insert(Class<T> type, Map fields);
+	<T extends Entity> T insert(T fields);
+	boolean delete(String table, Object pk);
+	boolean delete(Entity entity);
+	int deleteAll(String table, Map fields);
+	int deleteAll(Entity fields);
+	*/
 }
